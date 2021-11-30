58384014ecae7e0eb0c9888cbf9c682b86348556
Kevin Della Schiava <dellaschiavak@gmail.com>
2021-11-30 21:18:20 +0100

    Aggiunta funzione per scrivere la DCEL risultante su file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <queue>
 
 #include "OFFReader.cpp"
@@ -12,7 +13,7 @@ enum class event_type {
     merge
 };
 
-struct sweep_line_event {
+struct vertex_event {
     Vertex_handle vertex_handle;
     Halfedge_handle next_edge;
     Halfedge_handle prev_edge;
@@ -21,9 +22,49 @@ struct sweep_line_event {
 
 class EdgeStructure {
 private:
-    std::set<Halfedge_handle> edges;
+    class EdgeOrder {
 
+        Line_2& sl;
+
+    public:
+
+        EdgeOrder(Line_2& _sl) : sl(_sl) {}
+
+        bool operator() (Halfedge_handle i, Halfedge_handle j ) const {
+
+            const Point_2 *p = nullptr;
+            const Point_2 *q = nullptr;
+
+            CGAL::Object u = CGAL::intersection( CGAL::Line_2<Kernel>(i->source()->point(), i->target()->point()), sl );
+            CGAL::Object v = CGAL::intersection( CGAL::Line_2<Kernel>(j->source()->point(), j->target()->point()), sl );
+
+            if ( CGAL::object_cast<Point_2>(&u) ) {
+                p = CGAL::object_cast<Point_2>( &u );
+            } else if ( CGAL::object_cast<Line_2>(&u) ) {
+                p = (const Point_2*) i->source().ptr();
+            }
+            if ( CGAL::object_cast<Point_2>(&v) ) {
+                q = CGAL::object_cast<Point_2>( &v );
+            } else if ( CGAL::object_cast<Line_2>(&v) ) {
+                q = (const Point_2*) j->source().ptr();
+            }
+            if ( (p != nullptr) && (q != nullptr) ) {
+
+                return ( (*p).y() < (*q).y() );
+
+            } else {
+
+                return false;
+            }
+        }
+
+    };
+
+    Line_2 sl;
+    std::multiset<Halfedge_handle, EdgeOrder> edges;
 public:
+
+    EdgeStructure() : edges((EdgeOrder(sl))) {}
 
     void insert(const Halfedge_handle& e){
         edges.insert(e);
@@ -31,77 +72,21 @@ public:
     void erase(const Halfedge_handle& e){
         edges.erase(e);
     }
-
-    Halfedge_handle find_edge_underneath(const Vertex_handle& v){
-
-        Halfedge_handle e;
-        float min_distance = std::numeric_limits<float>::max();
-
-        for(const auto edge : edges){
-
-            const auto line = Line_2(v->point(), Direction_2(0, 1));
-            CGAL::Object u = CGAL::intersection(edge->curve().line(), line);
-            if(CGAL::object_cast<Point_2>(&u)){
-                Point_2 intersection_point = *CGAL::object_cast<Point_2>(&u);
-                if(intersection_point.y() < v->point().y()){
-                    if(v->point().y() - intersection_point.y() < min_distance){
-                        min_distance = v->point().y() - intersection_point.y();
-                        e = edge;
-                    }
-                }
-            }
-
-        }
-
-        return e;
+    Halfedge_handle find_edge_underneath(const Halfedge_handle& e){
+        sl = Line_2(e->source()->point(), Direction_2(0, 1));
+        const auto it = edges.upper_bound(e);
+        return *std::prev(it);
     }
 
-    void print_content(){
-        for(const auto e : edges){
-            std::cout << e->source()->point().x() << " " << e->source()->point().y();
-            std::cout << " -> ";
-            std::cout << e->target()->point().x() << " " << e->target()->point().y() << "\n";
-            std::cout << std::endl;
+    friend std::ostream& operator<<(std::ostream& out, const EdgeStructure& edge_structure){
+        out << "-----------------\n";
+        for(const auto e : edge_structure.edges){
+            out << e->source()->point().x() << " " << e->source()->point().y();
+            out << " -> ";
+            out << e->target()->point().x() << " " << e->target()->point().y();
+            out << "\n";
         }
-    }
-
-};
-
-
-class EdgeOrder {
-
-    Line_2& sl;
-
-public:
-
-    EdgeOrder(Line_2& _sl) : sl(_sl) {}
-
-    bool operator() (Halfedge_handle i, Halfedge_handle j ) const {
-
-        const Point_2 *p = nullptr;
-        const Point_2 *q = nullptr;
-
-        CGAL::Object u = CGAL::intersection( CGAL::Line_2<Kernel>(i->source()->point(), i->target()->point()), sl );
-        CGAL::Object v = CGAL::intersection( CGAL::Line_2<Kernel>(j->source()->point(), j->target()->point()), sl );
-
-        if ( CGAL::object_cast<Point_2>(&u) ) {
-            p = CGAL::object_cast<Point_2>( &u );
-        } else if ( CGAL::object_cast<Line_2>(&u) ) {
-            p = (const Point_2*) i->source().ptr();
-        }
-        if ( CGAL::object_cast<Point_2>(&v) ) {
-            q = CGAL::object_cast<Point_2>( &v );
-        } else if ( CGAL::object_cast<Line_2>(&v) ) {
-            q = (const Point_2*) j->source().ptr();
-        }
-        if ( (p != nullptr) && (q != nullptr) ) {
-
-            return ( (*p).y() < (*q).y() );
-
-        } else {
-
-            return false;
-        }
+        return out;
     }
 
 };
@@ -123,6 +108,7 @@ void remove(const Halfedge_handle& e,
 
     if(types.contains(u) && types.at(u) == event_type::merge){
         //Add diagonal to dcel from u to v
+        std::cout << "Inserting edge: " << u->point().x() << " " << u->point().y() << " -> " << v->point().x() << " " << v->point().y() << "\n";
         Segment_2 segment(u->point(), v->point());
         dcel.insert_at_vertices(segment, u, v);
     }
@@ -130,16 +116,18 @@ void remove(const Halfedge_handle& e,
     sweep_line_edges.erase(e);
 }
 void process(const Vertex_handle& v,
+             const Halfedge_handle& e,
              EdgeStructure& sweep_line_edges,
              std::map<Halfedge_const_handle, Vertex_handle>& helpers,
              const std::map<Vertex_const_handle, event_type>& types,
              CGAL::Arrangement_2<CGAL::Arr_segment_traits_2<Kernel>>& dcel){
 
-    const auto b = sweep_line_edges.find_edge_underneath(v);
+    const auto b = sweep_line_edges.find_edge_underneath(e);
 
     const auto& u = helpers.at(b);
     if(types.at(u) == event_type::merge || types.at(v) == event_type::split){
         //Add diagonal
+        std::cout << "Inserting edge: " << u->point().x() << " " << u->point().y() << " -> " << v->point().x() << " " << v->point().y() << "\n";
         Segment_2 segment(u->point(), v->point());
         dcel.insert_at_vertices(segment, u, v);
     }
@@ -147,82 +135,82 @@ void process(const Vertex_handle& v,
     helpers[b] = v;
 }
 
+vertex_event identify_event(const Halfedge_handle& edge_handle){
+    auto edge = *(edge_handle->twin().ptr());
+
+    auto vertex = edge.source();
+    const auto next_vertex = edge.target();
+    const auto prev_vertex = edge.prev()->source();
+
+    const auto point = vertex->point();
+    const auto next_point = next_vertex->point();
+    const auto prev_point = prev_vertex->point();
+
+    vertex_event event;
+    event.vertex_handle = vertex;
+    event.next_edge = edge_handle->twin();
+    event.prev_edge = edge_handle->twin()->prev();
+
+    if (CGAL::lexicographically_xy_smaller(prev_point, point) &&
+        CGAL::lexicographically_xy_smaller(point, next_point)) {
+        //lower regular
+        event.event_type = event_type::lower_regular;
+    }
+    if (CGAL::lexicographically_xy_larger(prev_point, point) &&
+        CGAL::lexicographically_xy_larger(point, next_point)) {
+        //upper regular
+        event.event_type = event_type::upper_regular;
+    }
+    if (CGAL::lexicographically_xy_larger(prev_point, point) &&
+        CGAL::lexicographically_xy_larger(next_point, point)) {
+        //Based on turn direction identify start or split event
+        if (CGAL::left_turn(prev_point, point, next_point)) {
+            event.event_type = event_type::start;
+        } else {
+            event.event_type = event_type::split;
+        }
+    }
+    if (CGAL::lexicographically_xy_smaller(prev_point, point) &&
+        CGAL::lexicographically_xy_smaller(next_point, point)) {
+        //Based on turn direction identify end or merge event
+        if (CGAL::left_turn(prev_point, point, next_point)) {
+            event.event_type = event_type::end;
+        } else {
+            event.event_type = event_type::merge;
+        }
+    }
+
+    return event;
+}
+
 void process_dcel(CGAL::Arrangement_2<CGAL::Arr_segment_traits_2<Kernel>>& dcel){
 
-    //Monotone subdivision of polygon
+    //Per ogni vertice nella DCEL registriamo il tipo di evento
     std::map<Vertex_const_handle, event_type> types;
-    //Events
-    const auto sweep_line_event_comparator = [](const auto &a, const auto &b) {
-        return CGAL::lexicographically_xy_larger(a.vertex_handle->point(), b.vertex_handle->point());
-    };
-    std::priority_queue<sweep_line_event, std::vector<sweep_line_event>, decltype(sweep_line_event_comparator)> vertex_events(
-            sweep_line_event_comparator);
 
+    std::vector<vertex_event> vertex_events;
+    vertex_events.reserve(dcel.number_of_vertices());
+
+    //Identificazione degli eventi associati a ciascun vertice
+    //Per processare tutti i vertici una volta sola processiamo un edge alla volta
+    //tenendo come vertice la sorgente dell'edge
+    //Gli edge hanno puntatori ai vertici ma non viceversa
     for(auto& edge_handle : dcel.edge_handles()){
+        const auto& event = identify_event(edge_handle);
 
-        auto edge = *(edge_handle->twin().ptr());
-
-        auto vertex = edge.source();
-        const auto next_vertex = edge.target();
-        const auto prev_vertex = edge.prev()->source();
-
-        const auto point = vertex->point();
-        const auto next_point = next_vertex->point();
-        const auto prev_point = prev_vertex->point();
-
-        sweep_line_event event;
-        event.vertex_handle = vertex;
-        event.next_edge = edge_handle->twin();
-        event.prev_edge = edge_handle->twin()->prev();
-
-        if (CGAL::lexicographically_xy_smaller(prev_point, point) &&
-            CGAL::lexicographically_xy_smaller(point, next_point)) {
-            //lower regular
-            event.event_type = event_type::lower_regular;
-        }
-        if (CGAL::lexicographically_xy_larger(prev_point, point) &&
-            CGAL::lexicographically_xy_larger(point, next_point)) {
-            //upper regular
-            event.event_type = event_type::upper_regular;
-        }
-        if (CGAL::lexicographically_xy_larger(prev_point, point) &&
-            CGAL::lexicographically_xy_larger(next_point, point)) {
-            //Based on turn direction identify start or split event
-            if (CGAL::left_turn(prev_point, point, next_point)) {
-                event.event_type = event_type::start;
-            } else {
-                event.event_type = event_type::split;
-            }
-        }
-        if (CGAL::lexicographically_xy_smaller(prev_point, point) &&
-            CGAL::lexicographically_xy_smaller(next_point, point)) {
-            //Based on turn direction identify end or merge event
-            if (CGAL::left_turn(prev_point, point, next_point)) {
-                event.event_type = event_type::end;
-            } else {
-                event.event_type = event_type::merge;
-            }
-        }
-
-        vertex_events.push(event);
-        types[vertex] = event.event_type;
+        vertex_events.push_back(event);
+        types[edge_handle->source()] = event.event_type;
     }
+
+    std::sort(vertex_events.begin(), vertex_events.end(), [](const auto& a, const auto& b){
+        return CGAL::lexicographically_xy_smaller(a.vertex_handle->point(), b.vertex_handle->point());
+    });
 
     std::map<Halfedge_const_handle, Vertex_handle> helpers;
 
-    Line_2 sl;
     EdgeStructure sweep_line_edges;
 
-    while (!vertex_events.empty()) {
-        auto &event = vertex_events.top();
-
-        const auto prev_edge_source_point = event.prev_edge->source()->point();
-        const auto prev_edge_target_point = event.prev_edge->target()->point();
-
-        const auto next_edge_source_point = event.next_edge->source()->point();
-        const auto next_edge_target_point = event.next_edge->target()->point();
-
-        sweep_line_edges.print_content();
+    for(const auto& event : vertex_events) {
 
         if (event.event_type == event_type::start) {
             insert(event.next_edge, sweep_line_edges, helpers, types);
@@ -235,18 +223,17 @@ void process_dcel(CGAL::Arrangement_2<CGAL::Arr_segment_traits_2<Kernel>>& dcel)
             insert(event.next_edge, sweep_line_edges, helpers, types);
         }
         if (event.event_type == event_type::upper_regular) {
-            process(event.vertex_handle, sweep_line_edges, helpers, types, dcel);
+            process(event.vertex_handle, event.next_edge, sweep_line_edges, helpers, types, dcel);
         }
         if (event.event_type == event_type::split) {
-            process(event.vertex_handle, sweep_line_edges, helpers, types, dcel);
+            process(event.vertex_handle, event.next_edge, sweep_line_edges, helpers, types, dcel);
             insert(event.next_edge, sweep_line_edges, helpers, types);
         }
         if (event.event_type == event_type::merge) {
             remove(event.prev_edge, event.vertex_handle, sweep_line_edges, helpers, types, dcel);
-            process(event.vertex_handle, sweep_line_edges, helpers, types, dcel);
+            process(event.vertex_handle, event.next_edge, sweep_line_edges, helpers, types, dcel);
         }
 
-        vertex_events.pop();
     }
 
 }
@@ -267,6 +254,49 @@ void print_dcel(CGAL::Arrangement_2<CGAL::Arr_segment_traits_2<Kernel>>& dcel){
 
 }
 
+void output_dcel(const std::string_view filename, CGAL::Arrangement_2<CGAL::Arr_segment_traits_2<Kernel>>& dcel){
+
+    std::ofstream off_file(filename.data());
+
+    const std::string off_file_start_token = "OFF";
+    off_file << off_file_start_token << "\n";
+
+    unsigned int num_vertices = dcel.number_of_vertices();
+    unsigned int num_faces = dcel.number_of_faces();
+    unsigned int num_edges = dcel.number_of_edges();
+
+    off_file << num_vertices << " " << num_faces << " " << num_edges << "\n";
+
+    //Map each vertex to its index
+    std::map<Point_2, int> vertices;
+    int vertex_index = 1;
+    for(const auto& vh : dcel.vertex_handles()){
+        vertices[vh->point()] = vertex_index;
+        off_file << vh->point().x() << " " << vh->point().y() << "\n";
+        vertex_index += 1;
+    }
+
+    for(const auto& face : dcel.face_handles()){
+        if(face->is_unbounded()) continue;
+
+        int n_of_edges_for_face = 0;
+        auto edge_circulator = face->outer_ccb();
+        do {
+            n_of_edges_for_face += 1;
+        } while (++edge_circulator != face->outer_ccb());
+
+        off_file << n_of_edges_for_face << " ";
+
+        edge_circulator = face->outer_ccb();
+        do {
+            off_file << vertices[edge_circulator->source()->point()] << " ";
+        } while (++edge_circulator != face->outer_ccb());
+
+        off_file << "\n";
+    }
+
+}
+
 int main() {
 
     //Example simple polygon
@@ -274,22 +304,24 @@ int main() {
 
     CGAL::Arrangement_2<CGAL::Arr_segment_traits_2<Kernel>> dcel;
 
+    //Creazione dei segmenti
     std::vector<Segment_2> dcel_segments;
-
     dcel_segments.reserve(polygon.edges.size());
-
     std::transform(polygon.edges.begin(), polygon.edges.end(), std::back_inserter(dcel_segments), [&](const auto& pair){
         const auto a = polygon.vertices[pair.first];
         const auto b = polygon.vertices[pair.second];
         return Segment_2(a, b);
     });
 
+    //Posizionamento di tutti i vertici nella faccia infinita
     std::map<Point_2, Vertex_handle> dcel_vertices;
     for(const auto& vertex : polygon.vertices){
+        //Salviamo i vertex_handle in una mappa per connetterli facilmente con i segmenti
         dcel_vertices[vertex] = dcel.insert_in_face_interior(vertex, dcel.unbounded_face());
     }
-    for(int i = 0; i < dcel_segments.size(); ++i){
-        dcel.insert_at_vertices(dcel_segments[i], dcel_vertices[dcel_segments[i].source()], dcel_vertices[dcel_segments[i].target()]);
+    for(auto & dcel_segment : dcel_segments){
+        //Per ogni segmento connettiamo i corrispondenti vertici
+        dcel.insert_at_vertices(dcel_segment, dcel_vertices[dcel_segment.source()], dcel_vertices[dcel_segment.target()]);
     }
 
     print_dcel(dcel);
@@ -297,6 +329,8 @@ int main() {
     process_dcel(dcel);
 
     print_dcel(dcel);
+
+    output_dcel("output.off", dcel);
 
     return 0;
 }
